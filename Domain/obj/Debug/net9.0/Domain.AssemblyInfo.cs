f58f28406b2b408e6f96643b803691a6d244b34f
EP <pinedajaja0@gmail.com>
2025-11-05 17:09:51 -0400

    new class in entities

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78b6e3d04c7d2e34650bf881c51333a67c432978")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3223830ded032c9ea3d0453656bf766e136d692c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
